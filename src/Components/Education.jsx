e1988d1bdf0797e743fd66f538854f59e45eb84c
TamilD07 <tamilkumaranmtk@gmail.com>
2024-12-26 12:32:00 +0530

    corrected the error

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -50,11 +50,11 @@ function Education() {
         {/* First Card */}
         {visibleCards.includes(0) && (
           <div className="edu-card flip-animation">
-<<<<<<< HEAD
+ 
             <img loading="lazy" className="card-img" src={img1} alt="college_picture" />
-=======
+
             <img className="card-img" src={img1} alt="college_picture" />
->>>>>>> ebaa28c56721f8f2d586c6946d6760aa2832ecde
+
             <div className="card-content">
               <h3>Under-Graduate 2024</h3>
               <h6>BSC.Computer Science</h6>
@@ -68,11 +68,10 @@ function Education() {
         {/* Second Card */}
         {visibleCards.includes(1) && (
           <div className="edu-card flip-animation">
-<<<<<<< HEAD
-            <img loading="lazy" className="card-img" src={img3} alt="school_picture" />
-=======
+           <img loading="lazy" className="card-img" src={img3} alt="school_picture" />
+
             <img className="card-img" src={img3} alt="school_picture" />
->>>>>>> ebaa28c56721f8f2d586c6946d6760aa2832ecde
+
             <div className="card-content">
               <h3>HSC</h3>
               <h6>Tamil Nadu Higher Secondary Certificate Examination</h6>
@@ -86,11 +85,11 @@ function Education() {
         {/* Third Card */}
         {visibleCards.includes(2) && (
           <div className="edu-card flip-animation">
-<<<<<<< HEAD
+
             <img loading="lazy" className="card-img" src={img4} alt="school_picture" />
-=======
+
             <img className="card-img" src={img4} alt="school_picture" />
->>>>>>> ebaa28c56721f8f2d586c6946d6760aa2832ecde
+
             <div className="card-content">
               <h3>SSLC</h3>
               <h6>Tamil Nadu Higher Secondary Certificate Examination</h6>
@@ -104,11 +103,10 @@ function Education() {
         {/* Fourth Card */}
         {visibleCards.includes(3) && (
           <div className="edu-card flip-animation">
-<<<<<<< HEAD
+
             <img loading="lazy" className="card-img" src={img5} alt="besant_technologies" />
-=======
             <img className="card-img" src={img5} alt="besant_technologies" />
->>>>>>> ebaa28c56721f8f2d586c6946d6760aa2832ecde
+
             <div className="card-content">
               <h3>FULL-STACK JAVA</h3>
               <h6>BESANT TECHNOLOGIES</h6>
--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import "../Style/Home.css";
 import image from "../images/image.png"; 
-<<<<<<< HEAD
-import Pdf from "../PDF/TamilKumaran_Full-Stack_resume.pdf"
-=======
-import Pdf from "../PDF/Tamil_Junior web developer_resume.pdf"
->>>>>>> ebaa28c56721f8f2d586c6946d6760aa2832ecde
+
+import Pdf from "../PDF/TamilKumaran_Full-Stack_resume.pdf";
+ 
+
 
 function Home() {  
   return (
@@ -19,18 +18,18 @@ function Home() {
             </div>
             <div className="in2 text-center">
               <p>
-<<<<<<< HEAD
+
               I’m a passionate junior web developer dedicated to creating impactful and responsive web solutions. 
               Proficient in HTML, CSS, JavaScript, and SQL databases, I excel in writing clean, efficient code and crafting intuitive user experiences. 
               My focus on innovation and quality ensures every project delivers value and stands apart. 
               Explore my work to see how I can help bring your vision to life. 
               Let’s build something extraordinary together!
-=======
+
                 I’m a passionate junior web developer with experience in creating dynamic and responsive web.
                 My expertise spans HTML, CSS, JavaScript, and SQL databases, with a focus on clean, efficient code and intuitive user experiences.
                 Explore my projects to see how I can bring value to your team through innovative solutions and a strong commitment to quality.
                 Let’s build something great together!
->>>>>>> ebaa28c56721f8f2d586c6946d6760aa2832ecde
+
               </p>
               <div className='resume-button'>
                 <a href={Pdf} download="TamilKumaran_Resume.pdf"  >
@@ -40,11 +39,11 @@ function Home() {
             </div>
           </div>
           <div className="image-container">
-<<<<<<< HEAD
+
             <img loading="lazy" src={image} alt="Profile" className="img-fluid" />  {/* Updated the image path */}
-=======
+
             <img src={image} alt="Profile" className="img-fluid" />  {/* Updated the image path */}
->>>>>>> ebaa28c56721f8f2d586c6946d6760aa2832ecde
+
           </div>
         </div>
       </div>
